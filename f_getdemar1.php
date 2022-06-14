da44e32cdd7fe4782c1f59e605fb720379639576
abusatch <abusatch@gmail.com>
2022-06-14 10:15:27 +0700

    okee1

--- f_getdemar1.php
+++ f_getdemar1.php
@@ -5,13 +5,48 @@ include('db.php');
 $no = 1;
 
 
+$ew = mysqli_query($koneksi,"select * from tb_menu where jenis = 'DEMARCHES' and id_menu = '69' ");
+
+$ew2 = mysqli_fetch_array($ew);
+?>
+
+{
+"id_actualite":"<?php echo $ew2['id_menu'] ?>",
+"id_actualite2":"<?php echo $ew2['id_menu'] ?>",
+"url":"<?php echo $ew2['url'] ?>",
+"menu_bg" : "<?php echo $ew2['menu_bg'] ?>",
+"menu_drop" : "<?php echo $ew2['menu_drop1'] ?>",
+"gambar":"https://ufe-section-indonesie.org/ufeapp/images/menu/<?php echo $ew2['gambar2'] ?>",
+
+"judul":"<?php echo $ew2['nama_menu'] ?>",
+"tree":[
+    <?php 
+    $nooo = 1;
+        $nk = mysqli_query($koneksi,"select * from tb_demar2 where id_kategori = '$ew2[id_menu]' and visibility = '1'");
+        while($nk2 = mysqli_fetch_array($nk)){
+            if($nooo == 1){}else{echo ",";}
+        ?>
+           { "judull" : "<?php echo $nk2['judul'] ?>",
+            "gambarr" : "https://ufe-section-indonesie.org/ufeapp/images/menu/<?php echo $nk2['gambar'] ?>",
+            "id_kategorii" : "<?php echo $nk2['id_kategori'] ?>"
+        }
+        
+           <?php $nooo++;} ?>
+                    ],
+"deskripsi":"<?php echo $desk7; ?>"
+
+}
+
+<?php
+
+
 if(!empty($_GET['jenis'])){
 $ew = mysqli_query($koneksi,"select * from tb_menu where jenis = '$_GET[jenis]' ");
 }else{
-$ew = mysqli_query($koneksi,"select * from tb_menu where jenis = 'DEMARCHES' ");
+$ew = mysqli_query($koneksi,"select * from tb_menu where jenis = 'DEMARCHES' and id_menu != '69' ");
 }
 while($ew2 = mysqli_fetch_array($ew)){
-if($no == 1){}else{echo ",";}
+if($no == 1){echo ",";}else{echo ",";}
 
 $desk1 = str_replace('\n',"",$ew2['short_desc']);
 $desk2 = str_replace("'","`",$desk1);
