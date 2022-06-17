e5f34e2f929db1ba9ac922db34cfc5759a845363
ardiarna <ardiarna17@gmail.com>
2022-06-17 16:58:21 +0700

    f_getdemart deskripsi 69

--- f_getdemar1.php
+++ f_getdemar1.php
@@ -8,6 +8,13 @@ $no = 1;
 $ew = mysqli_query($koneksi,"select * from tb_menu where jenis = 'DEMARCHES' and id_menu = '69' ");
 
 $ew2 = mysqli_fetch_array($ew);
+$desk1 = str_replace('\n',"",$ew2['short_desc']);
+$desk2 = str_replace("'","`",$desk1);
+$desk3 = str_replace('"',"`",$desk2);
+$desk4 = str_replace(str_split('\\/:*?"<>|'), ' ', $desk3);
+$desk5 = trim(preg_replace('/\s\s+/', ' ', $desk4));
+$desk6 = str_replace("<br>","",$desk5);
+$desk7 = str_replace(".","",$desk6);
 ?>
 
 {
