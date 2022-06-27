549476f25bb6490e4c7d290ce478a369b51095af
ardiarna <ardiarna17@gmail.com>
2022-06-27 10:16:17 +0700

    getdemar sort

--- f_getdemar1.php
+++ f_getdemar1.php
@@ -2,94 +2,48 @@
 
 echo '[';
 include('db.php');
-$no = 1;
-
-
-$ew = mysqli_query($koneksi,"select * from tb_menu where jenis = 'DEMARCHES' and id_menu = '69' ");
-
-$ew2 = mysqli_fetch_array($ew);
-$desk1 = str_replace('\n',"",$ew2['short_desc']);
-$desk2 = str_replace("'","`",$desk1);
-$desk3 = str_replace('"',"`",$desk2);
-$desk4 = str_replace(str_split('\\/:*?"<>|'), ' ', $desk3);
-$desk5 = trim(preg_replace('/\s\s+/', ' ', $desk4));
-$desk6 = str_replace("<br>","",$desk5);
-$desk7 = str_replace(".","",$desk6);
-?>
-
-{
-"id_actualite":"<?php echo $ew2['id_menu'] ?>",
-"id_actualite2":"<?php echo $ew2['id_menu'] ?>",
-"url":"<?php echo $ew2['url'] ?>",
-"menu_bg" : "<?php echo $ew2['menu_bg'] ?>",
-"menu_drop" : "<?php echo $ew2['menu_drop1'] ?>",
-"gambar":"https://ufe-section-indonesie.org/ufeapp/images/menu/<?php echo $ew2['gambar2'] ?>",
-
-"judul":"<?php echo $ew2['nama_menu'] ?>",
-"tree":[
-    <?php 
-    $nooo = 1;
-        $nk = mysqli_query($koneksi,"select * from tb_demar2 where id_kategori = '$ew2[id_menu]' and visibility = '1'");
-        while($nk2 = mysqli_fetch_array($nk)){
-            if($nooo == 1){}else{echo ",";}
-        ?>
-           { "judull" : "<?php echo $nk2['judul'] ?>",
-            "gambarr" : "https://ufe-section-indonesie.org/ufeapp/images/menu/<?php echo $nk2['gambar'] ?>",
-            "id_kategorii" : "<?php echo $nk2['id_kategori'] ?>"
-        }
-        
-           <?php $nooo++;} ?>
-                    ],
-"deskripsi":"<?php echo $desk7; ?>"
-
-}
-
-<?php
-
 
 if(!empty($_GET['jenis'])){
-$ew = mysqli_query($koneksi,"select * from tb_menu where jenis = '$_GET[jenis]' ");
+    $ew = mysqli_query($koneksi,"select * from tb_menu where jenis = '$_GET[jenis]' order by sort");
 }else{
-$ew = mysqli_query($koneksi,"select * from tb_menu where jenis = 'DEMARCHES' and id_menu != '69' ");
+    $ew = mysqli_query($koneksi,"select * from tb_menu where jenis = 'DEMARCHES' order by sort");
 }
+
 while($ew2 = mysqli_fetch_array($ew)){
-if($no == 1){echo ",";}else{echo ",";}
+    $desk1 = str_replace('\n',"",$ew2['short_desc']);
+    $desk2 = str_replace("'","`",$desk1);
+    $desk3 = str_replace('"',"`",$desk2);
+    $desk4 = str_replace(str_split('\\/:*?"<>|'), ' ', $desk3);
+    $desk5 = trim(preg_replace('/\s\s+/', ' ', $desk4));
+    $desk6 = str_replace("<br>","",$desk5);
+    $desk7 = str_replace(".","",$desk6);
+    ?>
+    {
+        "id_actualite":"<?php echo $ew2['id_menu'] ?>",
+        "id_actualite2":"<?php echo $ew2['id_menu'] ?>",
+        "url":"<?php echo $ew2['url'] ?>",
+        "menu_bg" : "<?php echo $ew2['menu_bg'] ?>",
+        "menu_drop" : "<?php echo $ew2['menu_drop1'] ?>",
+        "gambar":"https://ufe-section-indonesie.org/ufeapp/images/menu/<?php echo $ew2['gambar2'] ?>",
 
-$desk1 = str_replace('\n',"",$ew2['short_desc']);
-$desk2 = str_replace("'","`",$desk1);
-$desk3 = str_replace('"',"`",$desk2);
-$desk4 = str_replace(str_split('\\/:*?"<>|'), ' ', $desk3);
-$desk5 = trim(preg_replace('/\s\s+/', ' ', $desk4));
-$desk6 = str_replace("<br>","",$desk5);
-$desk7 = str_replace(".","",$desk6);
-?>
-{
-"id_actualite":"<?php echo $ew2['id_menu'] ?>",
-"id_actualite2":"<?php echo $ew2['id_menu'] ?>",
-"url":"<?php echo $ew2['url'] ?>",
-"menu_bg" : "<?php echo $ew2['menu_bg'] ?>",
-"menu_drop" : "<?php echo $ew2['menu_drop1'] ?>",
-"gambar":"https://ufe-section-indonesie.org/ufeapp/images/menu/<?php echo $ew2['gambar2'] ?>",
-
-"judul":"<?php echo $ew2['nama_menu'] ?>",
-"tree":[
-    <?php 
-    $nooo = 1;
-        $nk = mysqli_query($koneksi,"select * from tb_demar2 where id_kategori = '$ew2[id_menu]' and visibility = '1'");
-        while($nk2 = mysqli_fetch_array($nk)){
-            if($nooo == 1){}else{echo ",";}
-        ?>
-           { "judull" : "<?php echo $nk2['judul'] ?>",
-            "gambarr" : "https://ufe-section-indonesie.org/ufeapp/images/menu/<?php echo $nk2['gambar'] ?>",
-            "id_kategorii" : "<?php echo $nk2['id_kategori'] ?>"
-        }
-        
-           <?php $nooo++;} ?>
-                    ],
-"deskripsi":"<?php echo $desk7; ?>"
-
+        "judul":"<?php echo $ew2['nama_menu'] ?>",
+        "tree":[
+            <?php 
+            $nooo = 1;
+                $nk = mysqli_query($koneksi,"select * from tb_demar2 where id_kategori = '$ew2[id_menu]' and visibility = '1'");
+                while($nk2 = mysqli_fetch_array($nk)){
+                    if($nooo == 1){}else{echo ",";}
+                ?>
+                { "judull" : "<?php echo $nk2['judul'] ?>",
+                    "gambarr" : "https://ufe-section-indonesie.org/ufeapp/images/menu/<?php echo $nk2['gambar'] ?>",
+                    "id_kategorii" : "<?php echo $nk2['id_kategori'] ?>"
+                }
+                
+                <?php $nooo++;} ?>
+                            ],
+        "deskripsi":"<?php echo $desk7; ?>"
+    }
+<?php
 }
-<?php $no++;}
-
 echo "]";
 ?>
