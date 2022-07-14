4ba78c488da750f7b0361fd23815ebb6765c5e76
ardiarna <ardiarna17@gmail.com>
2022-07-14 14:29:03 +0700

    init search agent

--- reading_demarche.php
+++ reading_demarche.php
@@ -129,7 +129,7 @@ class ReadingDemarche
         a.nama_menu, a.gambar2 AS gambar_kategori 
       FROM tb_demar2 b
       JOIN tb_menu a ON(b.id_kategori = a.id_menu) 
-      WHERE b.searching = '1'";
+      WHERE b.visibility = '1' AND b.searching = '1'";
 
     $data = AFhelper::dbSelectAll($sql);
     $hasil = array();
@@ -150,6 +150,58 @@ class ReadingDemarche
       array_push($hasil, $a);
     }
       
+    AFhelper::kirimJson($hasil);
+  }
+
+  function searchagent() {
+    $sql = "SELECT a.id_agent, a.id_kategori, a.judul, a.judul2, a.short_desc, a.long_desc, a.gambar, a.gambar2, a.namaagent, a.gmaps, a.alamatagent, a.alamat2agent, 
+            a.kotaagent, a.kodeposagent, a.telpagent, a.mobileagent, a.emailagent, a.webagent, a.fbagent, a.twiteragent, a.igagent, a.playstoreagent,
+            a.rating1, a.rating2, a.rating3, a.visibility, b.judul AS judul_kategori, b.gambar AS gambar_kategori, c.id_menu, c.nama_menu AS judul_menu, c.gambar2 AS gambar_menu
+        FROM tb_agent a
+        JOIN tb_demar2 b ON(a.id_kategori = b.id_demar)
+        JOIN tb_menu c ON(b.id_kategori = c.id_menu)
+        WHERE a.visibility = '1' AND a.searching = '1'";
+
+    $data = AFhelper::dbSelectAll($sql);
+    $hasil = array();
+
+    foreach ($data as $row) {
+        $logo = $row->gambar ? "https://ufe-section-indonesie.org/ufeapp/images/agent/".$row->gambar : '';
+        $gambar = $row->gambar2 ? "https://ufe-section-indonesie.org/ufeapp/images/agent/".$row->gambar2 : '';
+        $gambar_kategori = $row->gambar_kategori ? "https://ufe-section-indonesie.org/ufeapp/images/menu/".$row->gambar_kategori : '';
+        $gambar_menu = $row->gambar_menu ? "https://ufe-section-indonesie.org/ufeapp/images/menu/".$row->gambar_menu : '';
+        $kota = str_replace($row->kodeposagent, "", $row->kotaagent);
+        $a = array(
+        "id_agent" => $row->id_agent,
+        "deskripsi" => AFhelper::formatText($row->long_desc),
+        "nama" => $row->namaagent,
+        "alamat" => AFhelper::formatText($row->alamatagent),
+        "kota" => $kota,
+        "kode_pos" => $row->kodeposagent,
+        "gmaps" => $row->gmaps,
+        "phone" => $row->telpagent,
+        "mobile" => $row->mobileagent,
+        "email" => $row->emailagent,
+        "web" => $row->webagent,
+        "facebook" => $row->fbagent,
+        "twitter" => $row->twiteragent,
+        "instagram" => $row->igagent,
+        "playstore" => $row->playstoreagent,
+        "logo" => $logo,
+        "gambar" => $gambar,
+        "rating1" => $row->rating1,
+        "rating2" => $row->rating2,
+        "rating3" => $row->rating3,
+        "id_kategori" => $row->id_kategori,
+        "judul_kategori" => $row->judul_kategori,
+        "gambar_kategori" => $gambar_kategori,
+        "id_menu" => $row->id_menu,
+        "judul_menu" => $row->judul_menu,
+        "gambar_menu" => $gambar_menu,
+        );
+        array_push($hasil, $a);
+    }
+        
     AFhelper::kirimJson($hasil);
   }
 
